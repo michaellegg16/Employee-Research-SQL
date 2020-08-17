f28ef4bf3a68b7acb5779092329c634219770d27
Michael Legg <mlegg@udel.edu>
2020-08-17 13:20:42 -0400

    SQL_Debugging

--- EmployeeSQL/employees_db.sql
+++ EmployeeSQL/employees_db.sql
@@ -3,8 +3,24 @@ CREATE TABLE departments(
 	dept_name varchar(40)
 );
 
-SELECT *
-FROM departments;
+SELECT * FROM departments;
+
+
+
+DROP  TABLE IF EXISTS employees CASCADE;
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	birth_date date,
+	first_name varchar(40),
+	last_name varchar(40),
+	gender varchar(20),
+	hire_date date
+);
+
+SELECT * FROM employees;
+
+
 
 DROP TABLE IF EXISTS dept_emp CASCADE;
 
@@ -13,12 +29,17 @@ CREATE TABLE dept_emp(
 	dept_no char(4),
 	from_date date,
 	to_date date,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT *
-FROM dept_emp
+ALTER TABLE dept_emp
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+ADD FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+;
+
+SELECT * FROM dept_emp
+
+
 
 DROP TABLE IF EXISTS dept_manager;
 
@@ -26,26 +47,17 @@ CREATE TABLE dept_manager(
 	dept_no char(4),
 	emp_no INT PRIMARY KEY,
 	from_date date,
-	to_date date,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	to_date date
 );
+
+ALTER TABLE dept_manager
+ADD FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+;
 
 SELECT * FROM dept_manager;
 
-DROP  TABLE employees CASCADE;
 
-CREATE TABLE employees(
-	emp_no INT PRIMARY KEY,
-	birth_date date,
-	first_name varchar(40),
-	last_name varchar(40),
-	gender varchar(20),
-	hire_date date,
-	FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
-);
-
-SELECT * FROM employees;
 
 DROP TABLE IF EXISTS salaries;
 
@@ -58,6 +70,8 @@ CREATE TABLE salaries(
 );
 
 SELECT * FROM salaries;
+
+
 
 DROP TABLE IF EXISTS titles;
 
